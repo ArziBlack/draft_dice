9e625d09329cd3c12ca0afd9085ef21a494f7088
MiltonBlack <arziblack2@gmail.com>
2024-09-22 18:46:04 +0100

    feat: initial app upgrade to v2...

--- src/navigation/StackNav.js
+++ src/navigation/StackNav.js
@@ -1,30 +1,8 @@
-import { View, Text, Image } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import BottomTabNav from './BottomTabNav'
 import Internet from '../screens/Internet'
-import NetInfo from '@react-native-community/netinfo'
-
-function Splash({ navigation }) {
-    const [isOffline, setIsOffline] = useState(false);
-    useEffect(() => {
-        const isOnline = NetInfo.addEventListener(state => {
-            state.isConnected ? setIsOffline(false) : setIsOffline(true);
-            const offline = !(state.isConnected);
-        })
-        return () => isOnline();
-    }, [])
-
-    setTimeout(() => {
-        isOffline ? navigation.navigate('Connection') : navigation.replace('Engineering Drawing Tutorials')
-    }, 3000)
-    return (
-        <View style={{ margin: 5 }}>
-            <Image source={require('./splash-image.png')} style={{ height: 300, width: 300, alignSelf: 'center' }} />
-            <Text style={{ fontSize: 20, textAlign: 'center', color: 'black', flexDirection: 'column', justifyContent: 'space-between', marginTop: '30%', paddingVertical: 30, paddingHorizontal: 8 }}>Engineering Drawing Tutorials by Black Dice Graphics</Text>
-        </View>
-    )
-}
+import Splash from '../screens/Splash'
 
 const Stack = createNativeStackNavigator();
 const StackNav = () => {
